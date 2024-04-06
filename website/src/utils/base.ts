683870ebf63edb805422fb95992d7fe6579d7b1a
Moritz Jung <m.projects.code@gmail.com>
2024-04-06 12:25:36 +0200

    use better fs path solution

--- website/src/utils/base.ts
+++ website/src/utils/base.ts
@@ -1,2 +1,10 @@
+// @ts-ignore
+import project from 'virtual:starlight/project-context';
+
 export const BASE_PATH = import.meta.url;
 // console.log(BASE_PATH);
+
+
+export function projectRelativeUrl(relativePath: string | URL): URL {
+    return new URL(relativePath, project.root);
+}
