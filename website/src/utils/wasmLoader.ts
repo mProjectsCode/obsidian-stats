12f0cc9e2de70811e37d591ba2f1c288a5fdf9df
Moritz Jung <m.projects.code@gmail.com>
2025-11-19 22:45:42 +0100

    update deps; fix wasm loader

--- website/src/utils/wasmLoader.ts
+++ website/src/utils/wasmLoader.ts
@@ -2,10 +2,20 @@ import init from '../../../data-wasm/pkg/data_wasm';
 // import wasmbin from '../../../data-wasm/pkg/data_wasm_bg.wasm?raw';
 import fs from 'fs/promises';
 
-export async function loadWasm() {
-	const wasmbin = await fs.readFile(new URL('../../../data-wasm/pkg/data_wasm_bg.wasm', import.meta.url));
+let wasm_promise: Promise<void> | undefined = undefined;
 
-	await init({ module_or_path: wasmbin as any });
+export async function loadWasm() {
+	if (wasm_promise) {
+		return wasm_promise;
+	} else {
+		wasm_promise = (async () => {
+			const wasmbin = await fs.readFile(new URL('../../../data-wasm/pkg/data_wasm_bg.wasm', import.meta.url));
+
+			await init({ module_or_path: wasmbin as any });
+		})();
+
+		return wasm_promise;
+	}
 }
 
 export * as wasm from '../../../data-wasm/pkg/data_wasm';
